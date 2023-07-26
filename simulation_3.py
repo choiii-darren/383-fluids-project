56cf187741ba797c7c68af01f88e39f1817f3699
Zereni <58187265+Zereni@users.noreply.github.com>
2023-07-26 12:17:04 -0400

    Polishing of Code + Explanation + Adding Sudden Expansion Head Loss

--- simulation_3.py
+++ simulation_3.py
@@ -29,9 +29,17 @@ def major_head_loss(velocity, tube_length, tube_diameter):
   if velocity == 0:
     return 0
   return (friction(velocity, tube_diameter) * tube_length * velocity **2) / (tube_diameter * 2 * g)
-  
+
+def minor_head_loss(velocity, k):
+  return (k * velocity ** 2) / (2 * g)
+
+def sudden_expansion_head_loss(tube1_velocity):
+  return ((tube1_velocity**2)/(2*g))*(1 - (tube1_area/tube2_area))**2
+
+
 def reynolds(velocity, tube_diameter):
   return (density_of_water * velocity * tube_diameter) / dynamic_viscosity_of_water
+
 
 def friction(velocity, tube_diameter):
   reynolds_num = reynolds(velocity, tube_diameter)
@@ -39,9 +47,9 @@ def friction(velocity, tube_diameter):
     return 64/reynolds_num
   else:
     return (1/(-1.8 * math.log10(((6.9)/reynolds_num) + (((tube_roughness/tube_diameter)/3.7)**1.11))))**2
-    
+
     # return 0.11 * (tube_roughness/original_diameter + 68/reynolds_num)**(0.25)
-  
+
     # def colebrook(friction, reynolds_num=reynolds_num):
     #   return -2 * math.log10(((tube_roughness/original_diameter)/3.7) + (2.51/(reynolds_num * math.sqrt(friction)))) - (1/math.sqrt(friction))
     # return fsolve(colebrook, 0.02)
@@ -49,10 +57,11 @@ def friction(velocity, tube_diameter):
 def second_tube_velocity(tube1_velocity):
   return (tube1_area / tube2_area) * tube1_velocity
 
-def minor_head_loss(velocity, k):
-  return (k * velocity ** 2) / (2 * g)
 
-
+# |__|============\\
+#                 ||
+# Tank -> Tube 1 -> Turn -> Tube 2 -> Exit
+# (v1) -> (v2)   -> (v2) -> (v3)   -> (v3)
 def simulation(tube1_length):
     time = 0  # Seconds elapsed from when we started draining
     time_step = 0.01  # (s)
@@ -62,17 +71,22 @@ def simulation(tube1_length):
     while (height > end_height):
         def velocity(tube1_velocity):
           tube2_velocity = second_tube_velocity(tube1_velocity)
-          major_head_loss_1 = major_head_loss(tube1_velocity, tube1_length, tube1_diameter)
-          minor_head_loss_1 = minor_head_loss(tube1_velocity,0.5)
-          major_head_loss_2 = major_head_loss(tube2_velocity, tube2_length, tube2_diameter)
-          minor_head_loss_2 = minor_head_loss(tube2_velocity,0.5)
-          bend_head_loss = minor_head_loss(max(tube2_velocity, tube1_velocity), 1.1)
-          if major_head_loss_1 + minor_head_loss_1 + major_head_loss_2 + minor_head_loss_2 + bend_head_loss > height:
-            return 10000 # Will indicate that this is not the root
-
-          return math.sqrt((2*g*(height - (major_head_loss_1 + minor_head_loss_1 + major_head_loss_2 + minor_head_loss_2 + bend_head_loss)))) - tube1_velocity
+          major_head_loss_1 = major_head_loss(tube1_velocity, tube1_length, tube1_diameter) #First we experience tube 1 friction
+          minor_head_loss_1 = minor_head_loss(tube1_velocity,0.5)                           #Then we experience tube 1 minor loss
+          bend_head_loss = minor_head_loss(tube1_velocity, 1.1)                             #Then I take a right angle turn
+          expansion_head_loss = sudden_expansion_head_loss(tube1_velocity)                  #Then I suddenly enter a large pipe
+          major_head_loss_2 = major_head_loss(tube2_velocity, tube2_length, tube2_diameter) #Then I experience tube 2 friction
+          minor_head_loss_2 = minor_head_loss(tube2_velocity,0.5)                           #Then I experience tube 2 minor loss
+          
+          
+          try:
+            return math.sqrt((2*g*(height - (major_head_loss_1 + minor_head_loss_1 + major_head_loss_2 + minor_head_loss_2 + bend_head_loss + expansion_head_loss)))) - tube1_velocity
+          except:
+            #fsolve approximates values for the velocity which can lead to math domain errors
+            #the below indicates that the answer is incorrect to fsolve
+            return 10000
         
-        v2 = fsolve(velocity, previous_exit_velocity)
+        v2 = fsolve(velocity, previous_exit_velocity) #Solve the velocity in tube1, using the previous velocity as a starting guess
         tank_loss_velocity = (tube1_area/container_area) * v2
         height_loss = tank_loss_velocity * time_step
 

55049bb36626a1d73440c75b194863c4060beca9
Zereni <58187265+Zereni@users.noreply.github.com>
2023-07-27 16:28:17 -0400

    Finalized Code - DO NOT CHANGE WITHOUT CONSULTATION

--- Submission Code/TJunction.py
+++ Submission Code/TJunction.py
@@ -1,31 +1,41 @@
 import math
 from scipy.optimize import fsolve
+import warnings
 
-# CONSTANTS
-g = 9.81  #[ms-2]
-dynamic_viscosity_of_water = 0.0010016  #[Pa*s]
-density_of_water = 997  #[kg/m^3]
+#ignores warnings
+warnings.filterwarnings("ignore")
+
+# WORLD CONSTANTS
+g = 9.81 #[m/s^2]
+dynamic_viscosity_of_water = 0.0010016 #[Pa*s]
+density_of_water = 997 #[kg/m^3]
 
 # PROBLEM CONSTANTS
-water_height_remaining = 0.08 #[m]
-tube_start_height_below_water_end = 0.02 #[m]
+start_height = 0.08 #[m]
+distance_to_tube = 0.02 #[m]
 container_length = 0.32 #[m]
 container_width = 0.13 #[m]
-sin_theta = float(1/150)  # Ratio
-
-tube_roughness = 0.0000015  #[m]
 container_area = container_length * container_width
+sin_theta = float(1/150) #[]
+original_diameter = 0.00794  #[m]
+original_tube_area = (math.pi * (1/4) * original_diameter ** 2) #[m^2]
 
-original_diameter = 0.00794  # [m]
-original_tube_area = (math.pi * (1/4) * original_diameter ** 2) #[m**2]
-tube1_area = original_tube_area / 2 #[m**2]
+# T-JUNCTION CONVERTED PROBLEM
+tube1_area = original_tube_area / 2 #[m^2]
 tube1_diameter = math.sqrt((tube1_area * 4) / math.pi) #[m]
 tube2_diameter = 0.0111125 #[m]
 tube2_area = (math.pi * (1/4) * tube2_diameter ** 2)
 tube2_length = 0.04 #[m]
 
+# CHOSEN VALUES
+tube_roughness = 0.0000025 #[m]
+time_step = 0.01
 
-def major_head_loss(velocity, tube_length, tube_diameter):
+
+def second_tube_velocity(tube1_velocity, tube1_area=tube1_area, tube2_area=tube2_area):
+  return (tube1_area / tube2_area) * tube1_velocity
+
+def major_head_loss(velocity, tube_length, tube_diameter=tube1_diameter):
   if velocity == 0:
     return 0
   return (friction(velocity, tube_diameter) * tube_length * velocity **2) / (tube_diameter * 2 * g)
@@ -33,15 +43,13 @@ def major_head_loss(velocity, tube_length, tube_diameter):
 def minor_head_loss(velocity, k):
   return (k * velocity ** 2) / (2 * g)
 
-def sudden_expansion_head_loss(tube1_velocity):
-  return ((tube1_velocity**2)/(2*g))*(1 - (tube1_area/tube2_area))**2
+def sudden_expansion_head_loss(tube_velocity, tube1_diameter=tube1_diameter, tube2_diameter=tube2_diameter):
+  return ((tube_velocity**2)/(2*g))*(1 - (tube1_diameter/tube2_diameter))
 
-
-def reynolds(velocity, tube_diameter):
+def reynolds(velocity, tube_diameter=tube1_diameter):
   return (density_of_water * velocity * tube_diameter) / dynamic_viscosity_of_water
 
-
-def friction(velocity, tube_diameter):
+def friction(velocity, tube_diameter=tube1_diameter):
   reynolds_num = reynolds(velocity, tube_diameter)
   if (reynolds_num <= 2300):
     return 64/reynolds_num
@@ -54,19 +62,10 @@ def friction(velocity, tube_diameter):
     #   return -2 * math.log10(((tube_roughness/original_diameter)/3.7) + (2.51/(reynolds_num * math.sqrt(friction)))) - (1/math.sqrt(friction))
     # return fsolve(colebrook, 0.02)
 
-def second_tube_velocity(tube1_velocity):
-  return (tube1_area / tube2_area) * tube1_velocity
-
-
-# |__|============\\
-#                 ||
-# Tank -> Tube 1 -> Turn -> Tube 2 -> Exit
-# (v1) -> (v2)   -> (v2) -> (v3)   -> (v3)
 def simulation(tube1_length):
     time = 0  # Seconds elapsed from when we started draining
-    time_step = 0.01  # (s)
-    height = water_height_remaining + tube_start_height_below_water_end + tube1_length*sin_theta
-    end_height = height - water_height_remaining
+    height = start_height + distance_to_tube + tube1_length*sin_theta
+    end_height = height - start_height
     previous_exit_velocity = math.sqrt(2*g*height)
     while (height > end_height):
         def velocity(tube1_velocity):
@@ -78,7 +77,6 @@ def simulation(tube1_length):
           major_head_loss_2 = major_head_loss(tube2_velocity, tube2_length, tube2_diameter) #Then I experience tube 2 friction
           minor_head_loss_2 = minor_head_loss(tube2_velocity,0.5)                           #Then I experience tube 2 minor loss
           
-          
           try:
             return math.sqrt((2*g*(height - (major_head_loss_1 + minor_head_loss_1 + major_head_loss_2 + minor_head_loss_2 + bend_head_loss + expansion_head_loss)))) - tube1_velocity
           except:
@@ -86,21 +84,21 @@ def simulation(tube1_length):
             #the below indicates that the answer is incorrect to fsolve
             return 10000
         
-        v2 = fsolve(velocity, previous_exit_velocity) #Solve the velocity in tube1, using the previous velocity as a starting guess
-        tank_loss_velocity = (tube1_area/container_area) * v2
+        tube1_exit_velocity = fsolve(velocity, previous_exit_velocity) #Solve the velocity in tube1, using the previous velocity as a starting guess
+        tank_loss_velocity = (tube1_area/container_area) * tube1_exit_velocity
         height_loss = tank_loss_velocity * time_step
 
         #Setup next step
         height -= height_loss
         time += time_step
-        previous_exit_velocity = v2
+        previous_exit_velocity = tube1_exit_velocity
 
     return time
 
 result_20cm = simulation(0.2)
 print(
-    f"LENGTH: 20 cm -> {result_20cm:.2f} seconds ({int(result_20cm//60)}:{round(result_20cm % 60):02}) -> 3:19")
+    f"LENGTH: 20 cm -> {result_20cm:.2f} seconds ({int(result_20cm//60)}:{round(result_20cm % 60):02}) -> Real Life (w/o T Junction): 3:19")
 
 result_40cm = simulation(0.4)
 print(
-    f"LENGTH: 40 cm -> {result_40cm:.2f} seconds ({int(result_40cm//60)}:{round(result_40cm % 60):02}) -> 4:26")
+    f"LENGTH: 40 cm -> {result_40cm:.2f} seconds ({int(result_40cm//60)}:{round(result_40cm % 60):02}) -> Real Life (w/o T Junction): 4:26")
--- Submission Code/noTJunction.py
+++ Submission Code/noTJunction.py
@@ -1,36 +1,44 @@
 import math
 from scipy.optimize import fsolve
 
-# CONSTANTS
-g = 9.81  #[ms-2]
-dynamic_viscosity_of_water = 0.0010016  #[Pa*s]
-density_of_water = 997  #[kg/m^3]
+# WORLD CONSTANTS
+g = 9.81 #[m/s^2]
+dynamic_viscosity_of_water = 0.0010016 #[Pa*s]
+density_of_water = 997 #[kg/m^3]
 
 # PROBLEM CONSTANTS
-water_height_remaining = 0.08 #[m]
-tube_start_height_below_water_end = 0.02 #[m]
+start_height = 0.08 #[m]
+distance_to_tube = 0.02 #[m]
 container_length = 0.32 #[m]
 container_width = 0.26 #[m]
-sin_theta = float(1/150)  # Ratio
-tube_diameter = 0.00794  #[m]
-tube_roughness = 0.0000015  #[m]
-container_area = container_length * container_width
-tube_area = math.pi * (1/4) * tube_diameter ** 2
+container_area = container_length * container_width #[m^2]
+sin_theta = float(1/150) #[]
+tube1_diameter = 0.00794 #[m]
+tube_area = math.pi * (1/4) * tube1_diameter ** 2 #[m^2]
 
-def head_loss(velocity, tube_length):
+# CHOSEN VALUES
+tube_roughness = 0.0000025 #[m]
+time_step = 0.01 #[s]
+
+
+def major_head_loss(velocity, tube1_length, tube_diameter=tube1_diameter):
   if velocity == 0:
     return 0
-  return (friction(velocity) * tube_length * velocity **2) / (tube_diameter * 2 * g)
-  
-def reynolds(velocity):
+  return (friction(velocity, tube_diameter) * tube1_length * velocity ** 2) / (tube_diameter * 2 * g)
+
+
+def minor_head_loss(velocity, k):
+  return (k * velocity ** 2) / (2 * g)
+
+def reynolds(velocity, tube_diameter=tube1_diameter):
   return (density_of_water * velocity * tube_diameter) / dynamic_viscosity_of_water
 
-def friction(velocity):
-  reynolds_num = reynolds(velocity)
+def friction(velocity, tube_diameter=tube1_diameter):
+  reynolds_num = reynolds(velocity, tube1_diameter)
   if (reynolds_num <= 2300):
     return 64/reynolds_num
   else:
-    return (1/(-1.8 * math.log10(((6.9)/reynolds_num) + (((tube_roughness/tube_diameter)/3.7)**1.11))))**2
+    return (1/(-1.8 * math.log10(((6.9)/reynolds_num) + (((tube_roughness/tube1_diameter)/3.7)**1.11))))**2
     
     # return 0.11 * (tube_roughness/tube_diameter + 68/reynolds_num)**(0.25)
   
@@ -38,54 +46,55 @@ def friction(velocity):
     #   return -2 * math.log10(((tube_roughness/tube_diameter)/3.7) + (2.51/(reynolds_num * math.sqrt(friction)))) - (1/math.sqrt(friction))
     # return fsolve(colebrook, 0.02)
 
-
-def simulation(tube_length):
-    time = 0  # Seconds elapsed from when we started draining
-    time_step = 0.01  # (s)
-    height = water_height_remaining + tube_start_height_below_water_end + tube_length*sin_theta
-    end_height = height - water_height_remaining
+def simulation(tube1_length):
+    time = 0  #Seconds elapsed from when we started draining
+    height = start_height + distance_to_tube + tube1_length*sin_theta
+    end_height = height - start_height
     previous_exit_velocity = math.sqrt(2*g*height)
     while (height > end_height):
-        def velocity(velocity):
-          major_head_loss = head_loss(velocity, tube_length)
-          minor_head_loss = (0.5 * velocity**2) / (2 * g)
-          if major_head_loss + minor_head_loss> height:
-            return 10000 # Will indicate that this is not the root
+        def velocity(tube1_velocity):
+          major_head_loss_1 = major_head_loss(tube1_velocity, tube1_length)
+          minor_head_loss_1 = minor_head_loss(tube1_velocity, 0.5)
 
-          return math.sqrt((2*g*(height - major_head_loss - minor_head_loss))) - velocity
-        
-        exit_velocity = fsolve(velocity, previous_exit_velocity)
-        tank_loss_velocity = (tube_area/container_area) * exit_velocity
+          try:
+            return math.sqrt((2*g*(height - major_head_loss_1 - minor_head_loss_1))) - tube1_velocity
+          except:
+            #fsolve approximates values for the velocity which can lead to math domain errors
+            #the below indicates that the answer is incorrect to fsolve
+            return 10000
+
+        tube1_exit_velocity = fsolve(velocity, previous_exit_velocity)
+        tank_loss_velocity = (tube_area/container_area) * tube1_exit_velocity
         height_loss = tank_loss_velocity * time_step
 
         #Setup next step
         height -= height_loss
         time += time_step
-        previous_exit_velocity = exit_velocity
+        previous_exit_velocity = tube1_exit_velocity
 
     return time
 
 result_10cm = simulation(0.1)
 print(
-    f"LENGTH: 10 cm -> {result_10cm:.2f} seconds ({int(result_10cm//60)}:{round(result_10cm % 60):02}) -> ??")
+    f"LENGTH: 10 cm -> {result_10cm:.2f} seconds ({int(result_10cm//60)}:{round(result_10cm % 60):02}) -> Real Life: ??")
 
 result_20cm = simulation(0.2)
 actual_20 = 199
 print(
-    f"LENGTH: 20 cm -> {result_20cm:.2f} seconds ({int(result_20cm//60)}:{round(result_20cm % 60):02}) -> 3:19")
+    f"LENGTH: 20 cm -> {result_20cm:.2f} seconds ({int(result_20cm//60)}:{round(result_20cm % 60):02}) -> Real Life: 3:19")
 
 result_30cm = simulation(0.3)
 actual_30 = 214
 print(
-    f"LENGTH: 30 cm -> {result_30cm:.2f} seconds ({int(result_30cm//60)}:{round(result_30cm % 60):02}) -> 3:34")
+    f"LENGTH: 30 cm -> {result_30cm:.2f} seconds ({int(result_30cm//60)}:{round(result_30cm % 60):02}) -> Real Life: 3:34")
 
 result_40cm = simulation(0.4)
 actual_40 = 266
 print(
-    f"LENGTH: 40 cm -> {result_40cm:.2f} seconds ({int(result_40cm//60)}:{round(result_40cm % 60):02}) -> 4:26")
+    f"LENGTH: 40 cm -> {result_40cm:.2f} seconds ({int(result_40cm//60)}:{round(result_40cm % 60):02}) -> Real Life: 4:26")
 
 result_60cm = simulation(0.6)
 actual_60 = 288
 print(
-    f"LENGTH: 60 cm -> {result_60cm:.2f} seconds ({int(result_60cm//60)}:{round(result_60cm % 60):02}) -> 4:48")
-print("Total Error: ", abs(result_20cm-actual_20) + abs(result_30cm-actual_30) + abs(result_40cm-actual_40) + abs(result_60cm-actual_60))
+    f"LENGTH: 60 cm -> {result_60cm:.2f} seconds ({int(result_60cm//60)}:{round(result_60cm % 60):02}) -> Real Life: 4:48")
+print("Total Absolute Error: ", abs(result_20cm-actual_20) + abs(result_30cm-actual_30) + abs(result_40cm-actual_40) + abs(result_60cm-actual_60))
